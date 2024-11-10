67c636fb698a03588e2a563600b63744e6b00f63
Rashid J. Almheiri <r.muhairi@pm.me>
2024-11-10 21:08:50 +0400

    update fleyk

--- flake.nix
+++ flake.nix
@@ -1,36 +1,20 @@
 {
-  description = "dev71/yap:";
+  description = "dev71/yap: Yet another chat protocol";
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [inputs.treefmt.flakeModule {perSystem = {lib, ...}: {_module.args.l = lib // builtins;};}];
-
-      perSystem = {
-        l,
-        pkgs,
-        config,
-        ...
-      }: {
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-          programs.alejandra.enable = true;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = l.attrValues {
-            inherit (pkgs) tlaplus18;
-            inherit (config.treefmt.build.programs) alejandra;
-          };
-        };
-      };
-
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = builtins.attrValues {
+            inherit (pkgs) tlaplus18;
+          };
+        }; # devShells.default
+      }; # perSystem
     };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    treefmt.url = "github:numtide/treefmt-nix";
-
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 }
